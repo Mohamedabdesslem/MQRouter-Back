e5c5f71399d1d4034872c58a9d8898f7e774af59
Mohamed Abdesslem Boudabbous <abdesslem.boudabbous@gmail.com>
2025-04-03 22:02:16 +0200

    Configuration CORS

--- src/main/java/com/bank/MQRouter/MQRouterApplication.java
+++ src/main/java/com/bank/MQRouter/MQRouterApplication.java
@@ -2,12 +2,31 @@ package com.bank.MQRouter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@Configuration
 public class MQRouterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MQRouterApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**") // Autoriser toutes les routes API
+						.allowedOrigins("http://localhost:4200") // Autoriser l'origine Angular
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Méthodes autorisées
+						.allowedHeaders("*") // Tous les headers
+						.allowCredentials(true); // Autoriser les cookies et authentifications
+			}
+		};
 	}
 
 }
